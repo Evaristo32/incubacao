2d56dec316e6b36cea001c000bd6014c435bbc61
tp.evaristo.soares <eq6njklk4gk5ftdppcyb5673566iavc3btviradhogghgdhssmiq>
2022-05-04 15:35:38 -0300

    Liberando cors - 01

--- src/main/java/br/com/incubacao/config/CorsConfig.java
+++ src/main/java/br/com/incubacao/config/CorsConfig.java
@@ -3,22 +3,23 @@ package br.com.incubacao.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig  implements WebMvcConfigurer {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
                 .allowedOrigins("*")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),  HttpMethod.DELETE.name(), HttpMethod.PATCH.name())
-                .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.ORIGIN, HttpHeaders.ACCEPT,HttpHeaders.CONTENT_TYPE)
+                        HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.PATCH.name())
+                .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.ORIGIN, HttpHeaders.ACCEPT, HttpHeaders.CONTENT_TYPE)
                 .exposedHeaders("header1", "header2")
-                .allowCredentials(true).maxAge(3600);
+                .allowCredentials(false).maxAge(3600);
     }
 
 }
